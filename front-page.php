08e8bea9b2e2bd928c7a584580dc024aef32b2cf
Daniel Parvin <danparvin@gmail.com>
2016-08-15 15:13:39 -0400

    Initial commit

---
+++ front-page.php
@@ -0,0 +1,41 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<title>Daniel Parvin</title>
+
+<link href="https://fonts.googleapis.com/css?family=Play:700" rel="stylesheet">
+<style type="text/css" media="all">
+body {
+  background: #795548;
+  font: 14px 'Play', sans-serif;
+  line-height: 1.4;
+  margin: 0;
+  padding: 2em;
+}
+
+h1 {
+  font-size: 2em;
+  margin: 0;
+  color: white;
+  text-align: center;
+}
+
+@media screen and (min-width: 768px) {
+  body {
+    padding: 5em;
+  }
+  h1 {
+    font-size: 3em;
+    margin-top: 50px;
+  }
+}
+</style>
+</head>
+
+<body>
+<h1>This page is under construction.</h1>
+</body>
+</html>
